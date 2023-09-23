53b89f83aaed051d33aeae904693939654af66f5
Garen Vartanian <garenv@gmail.com>
2023-09-23 15:37:35 -0700

    fix: cleanup and max safe size

--- contracts/CircularBufferLib.sol
+++ contracts/CircularBufferLib.sol
@@ -3,28 +3,30 @@ pragma solidity ^0.8.0;
 
 // CircularBuffer Library
 library CircularBufferLib {
+    constant uint256 ONE_HUNDRED = 100; // max 99 safe values
+
     struct Buffer {
-        uint256[100] data;
+        uint256[ONE_HUNDRED] data;
         uint256 head;
         uint256 tail;
     }
 
     function push(Buffer storage buf, uint256 value) internal {
         buf.data[buf.tail] = value;
-        buf.tail = (buf.tail + 1) % 100;
+        buf.tail = (buf.tail + 1) % ONE_HUNDRED;
 
         if (buf.tail == buf.head) {
-            buf.head = (buf.head + 1) % 100;
+            buf.head = (buf.head + 1) % ONE_HUNDRED;
         }
     }
 
     function getOrderedBuffer(
         Buffer storage buf
     ) internal view returns (uint256[] memory) {
-        uint256[] memory ordered = new uint256[100];
+        uint256[] memory ordered = new uint256[ONE_HUNDRED];
         uint256 count = 0;
 
-        for (uint256 i = buf.head; i != buf.tail; i = (i + 1) % 100) {
+        for (uint256 i = buf.head; i != buf.tail; i = (i + 1) % ONE_HUNDRED) {
             ordered[count] = buf.data[i];
             count++;
         }
@@ -41,11 +43,11 @@ library CircularBufferLib {
         Buffer storage buf,
         uint256 n
     ) internal view returns (uint256[] memory) {
-        if (n > 100) n = 100; // Limit to max buffer size
+        if (n > ONE_HUNDRED - 1) n = ONE_HUNDRED - 1; // Limit to max safe size
 
         uint256 availableValues = buf.tail >= buf.head
             ? buf.tail - buf.head
-            : 100 + buf.tail - buf.head;
+            : ONE_HUNDRED + buf.tail - buf.head;
 
         // Determine the number of values to retrieve
         uint256 valuesToRetrieve = n < availableValues ? n : availableValues;
@@ -56,7 +58,7 @@ library CircularBufferLib {
         int256 index = int256(buf.tail) - 1; // Start from tail and move backward
 
         while (count < valuesToRetrieve) {
-            if (index < 0) index = 99; // Wrap around if negative
+            if (index < 0) index = ONE_HUNDRED - 1; // Wrap around if negative
             latestValues[count] = buf.data[uint256(index)];
             count++;
             index--;
